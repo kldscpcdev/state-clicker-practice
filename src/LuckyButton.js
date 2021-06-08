970dcaac7a2dd90be5707db87e64ba8a0de85936
kldscpcdev <80473306+kldscpcdev@users.noreply.github.com>
2021-06-08 11:34:49 -0700

    added display change state when user randomly gets the number 7

--- src/LuckyButton.js
+++ src/LuckyButton.js
@@ -1,26 +1,37 @@
 import React, { Component } from "react";
 
-class Counter extends Component {
+class LuckyButton extends Component {
   constructor(props) {
     super(props);
+    this.randNum = this.randNum.bind(this);
+    this.areYouLucky = this.areYouLucky.bind(this);
     this.state = {
       num: 1, // start page with number 1
+      display: <button onClick={this.randNum}>Possibly a Lucky Number</button>, //starting display
     };
-    this.randNum = this.randNum.bind(this);
   }
   randNum() {
     this.setState({
       num: Math.floor(Math.random() * 10),
     });
+    this.areYouLucky();
+  }
+
+  areYouLucky() {
+    if (this.state.num === 7) {
+      this.setState({
+        display: "YOU ARE SOOOO LUCKY 🎰 🎉 🌟",
+      });
+    }
   }
   render() {
     return (
       <div>
         <div>Your number is {this.state.num}</div>
-        <button onClick={this.randNum}>Possibly a Lucky Number</button>
+        <div>{this.state.display}</div>
       </div>
     );
   }
 }
 
-export default Counter;
+export default LuckyButton;
