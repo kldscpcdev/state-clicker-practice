906b3a8a3065719270638757c02259d1eafb6a9f
kldscpcdev <80473306+kldscpcdev@users.noreply.github.com>
2021-06-08 12:09:32 -0700

    updated logic to fix "you are lucky" message not displaying until after second click

--- src/LuckyButton.js
+++ src/LuckyButton.js
@@ -4,31 +4,25 @@ class LuckyButton extends Component {
   constructor(props) {
     super(props);
     this.randNum = this.randNum.bind(this);
-    this.areYouLucky = this.areYouLucky.bind(this);
     this.state = {
       num: 1, // start page with number 1
-      display: <button onClick={this.randNum}>Possibly a Lucky Number</button>, //starting display
     };
   }
   randNum() {
+    let randomNum = Math.floor(Math.random() * 10) + 1;
     this.setState({
-      num: Math.floor(Math.random() * 10),
+      num: randomNum,
     });
-    this.areYouLucky();
-  }
-
-  areYouLucky() {
-    if (this.state.num === 7) {
-      this.setState({
-        display: "YOU ARE SOOOO LUCKY 🎰 🎉 🌟",
-      });
-    }
   }
   render() {
     return (
       <div>
         <div>Your number is {this.state.num}</div>
-        <div>{this.state.display}</div>
+        {this.state.num === 7 ? (
+          <div>YOU ARE SOOOO LUCKY 🎰 🎉 🌟 </div>
+        ) : (
+          <button onClick={this.randNum}>Possibly a Lucky Number</button>
+        )}
       </div>
     );
   }
